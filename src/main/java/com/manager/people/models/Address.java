70a96fb135b68952f5bcbb327e788246ed29b5db
TaylanTorres09 <taylantorres@gmail.com>
2023-03-02 15:15:20 -0300

    Add JsonIgnore

--- src/main/java/com/manager/people/models/Address.java
+++ src/main/java/com/manager/people/models/Address.java
@@ -1,5 +1,7 @@
 package com.manager.people.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -32,6 +34,7 @@ public class Address {
 
     private Boolean principalAddress;
 
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "person_id")
     private Person person;
