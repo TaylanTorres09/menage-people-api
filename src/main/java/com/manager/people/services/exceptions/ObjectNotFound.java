92a680b42cd2dede17611db426bfe40bbb8269e0
TaylanTorres09 <taylantorres@gmail.com>
2023-03-03 10:39:08 -0300

    Add service exception to Object Not Found

---
+++ src/main/java/com/manager/people/services/exceptions/ObjectNotFound.java
@@ -0,0 +1,9 @@
+package com.manager.people.services.exceptions;
+
+public class ObjectNotFound extends RuntimeException{
+
+    public ObjectNotFound(String arg0) {
+        super(arg0);
+    }
+    
+}
