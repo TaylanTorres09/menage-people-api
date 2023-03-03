49b8d46e446dc2a695858d97f85a56a0e8f71921
TaylanTorres09 <taylantorres@gmail.com>
2023-03-03 10:42:56 -0300

    create method to return PersonDTO

--- src/main/java/com/manager/people/services/PersonService.java
+++ src/main/java/com/manager/people/services/PersonService.java
@@ -1,11 +1,13 @@
 package com.manager.people.services;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.manager.people.dtos.PersonDTO;
 import com.manager.people.models.Person;
 import com.manager.people.repositories.PersonRepository;
+import com.manager.people.services.exceptions.ObjectNotFound;
 
 @Service
 public class PersonService {
@@ -13,8 +15,16 @@ public class PersonService {
     @Autowired
     private PersonRepository personRepository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     public Person findByID(Long id) {
-        return personRepository.findById(id).get();
+        return personRepository.findById(id).orElseThrow(() -> new ObjectNotFound("Person not found"));
+    }
+
+    public PersonDTO findByIdWithOutAddress(Long id) {
+        Person person = this.findByID(id);
+        return mapper.map(person, PersonDTO.class);
     }
 
 }
