3786b0ca178e6cd333cf0283e56736d4cefa2b12
TaylanTorres09 <taylantorres@gmail.com>
2023-03-04 15:36:51 -0300

    treatment error to invalid request

--- src/main/java/com/manager/people/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/manager/people/controllers/exceptions/ControllerExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.manager.people.controllers.exceptions;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,6 +20,16 @@ public class ControllerExceptionHandler {
     public ResponseEntity<StandardError> objectNotFound(ObjectNotFound e, ServletRequest request) {
         StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), e.getMessage());
         return new ResponseEntity<StandardError>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validationError(MethodArgumentNotValidException e, ServletRequest request) {
+        List<FieldError> listFieldError = e.getBindingResult().getFieldErrors();
+        String message = listFieldError.stream().map(er -> er.getDefaultMessage()).toList().get(0);
+
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), message);
+
+        return new ResponseEntity<StandardError>(error, HttpStatus.BAD_REQUEST);
     }
 
 }
