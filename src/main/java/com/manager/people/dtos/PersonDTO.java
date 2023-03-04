c9edbbf8a8fcae492b2c7db351a80a789d6d55dc
TaylanTorres09 <taylantorres@gmail.com>
2023-03-04 15:34:08 -0300

    Change type of atribute

--- src/main/java/com/manager/people/dtos/PersonDTO.java
+++ src/main/java/com/manager/people/dtos/PersonDTO.java
@@ -2,6 +2,9 @@ package com.manager.people.dtos;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,8 +16,12 @@ public class PersonDTO {
     
     private Long id;
 
+    @NotBlank(message = "Required Name")
+    @Size(min = 5, max = 20, message = "Length between 5 and 20 characters")
     private String name;
 
-    private LocalDate birthDate;
+    @NotBlank(message = "Required Birth Date")
+    @Pattern(regexp = "^([0-2][0-9]|(3)[0-1])(\\/)(((0)[0-9])|((1)[0-2]))(\\/)\\d{4}$", message = "example dd/mm/yyyy")
+    private String birthDate;
 
 }
